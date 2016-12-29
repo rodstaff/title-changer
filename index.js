8bcbf942cedde441d4168697e51eddab5beea4c2
rodk <rod2010rod2010@hotmail.com>
2016-12-28 21:54:41 -0500

    modified initial state content

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class TestApp extends React.Component {
   constructor(props) {
   	super(props);
   	this.state = {
-  	  name: this.props.initialName[0] + " " + this.props.initialName[1]
+  	  name: this.props.initialName[0] + " a.k.a. \"" + this.props.initialName[1] + "\""
   	};
   }
   handleChange(e) {
